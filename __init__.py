a32f9c6f3f5afed1f619172a56da39a033d26625
AlexRemstedt <alex@remstedt.com>
2021-07-06 23:49:38 +0200

    rename __main__.py

--- __init__.py
+++ __init__.py
@@ -5,5 +5,8 @@ Stocktracker
 Stocktracker is an stocktracker, written in python, which will help keep
 track of your stocks
 """
+__docformat__ = "restructuredtext"
 # Alex Remstedt
 from get_data import Share
+
+
