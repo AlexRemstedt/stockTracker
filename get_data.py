a415faf13cadb2bc3238489e638730a115153431
AlexRemstedt <alex@remstedt.com>
2021-07-06 23:50:35 +0200

    get_data: update

--- get_data.py
+++ get_data.py
@@ -12,14 +12,12 @@ import requests
 
 
 class Share():
-    """
-    """    
+    """Get data from shares."""    
 
     token = 'pk_dae64b222a3a401da19fa29c020c0897'  # publishable
     base_url = 'https://cloud.iexapis.com/'
 
-    def __init__(self, name, code) -> None:
-        self.name = name
+    def __init__(self, code) -> None:
         self.code = code
         self.url = 'https://cloud.iexapis.com/stable/stock/' \
             f'{self.code}/quote?token={self.token}'
@@ -32,5 +30,5 @@ class Share():
 
         
 if __name__ == "__main__":
-    apple = Share('Apple', 'AAPL')
+    apple = Share('AAPL')
     print(apple.get_data())
